9b7163bf3471696aaccec003996537a8454a7601
sun <asmh1989@gmail.com>
2018-07-27 09:33:09 +0800

    完善ios扫描界面

--- ios/Classes/BarcodeScannerViewController.h
+++ ios/Classes/BarcodeScannerViewController.h
@@ -2,15 +2,11 @@
 // Created by Matthew Smith on 11/7/17.
 //
 
-#import <Foundation/Foundation.h>
-#import <MTBBarcodeScanner/MTBBarcodeScanner.h>
-
+#import <UIKit/UIKit.h>
 #import "BarcodeScannerViewControllerDelegate.h"
 
 
 @interface BarcodeScannerViewController : UIViewController
-@property(nonatomic, retain) UIView *previewView;
-@property(nonatomic, retain) MTBBarcodeScanner *scanner;
 @property(nonatomic, weak) id<BarcodeScannerViewControllerDelegate> delegate;
 
 @end
--- ios/Classes/BarcodeScannerViewController.m
+++ ios/Classes/BarcodeScannerViewController.m
@@ -3,68 +3,145 @@
 //
 
 #import "BarcodeScannerViewController.h"
-#import <MTBBarcodeScanner/MTBBarcodeScanner.h>
+#import "SGQRCode.h"
+#import "SGQRCodeScanManager.h"
 
 
-@implementation BarcodeScannerViewController {
+@interface BarcodeScannerViewController ()<SGQRCodeScanManagerDelegate>{
 }
 
+@property (nonatomic, strong) SGQRCodeScanningView *scanningView;
+@property (nonatomic, strong) SGQRCodeScanManager *manager;
+@property (nonatomic, strong) UILabel *promptLabel;
+@end
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.previewView = [[UIView alloc] initWithFrame:self.view.bounds];
-    self.previewView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:_previewView];
-    [self.view addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"V:[previewView]"
-                                options:NSLayoutFormatAlignAllBottom
-                                metrics:nil
-                                  views:@{@"previewView": _previewView}]];
-    [self.view addConstraints:[NSLayoutConstraint
-            constraintsWithVisualFormat:@"H:[previewView]"
-                                options:NSLayoutFormatAlignAllBottom
-                                metrics:nil
-                                  views:@{@"previewView": _previewView}]];
-    self.scanner = [[MTBBarcodeScanner alloc] initWithPreviewView:_previewView];
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel)];
-}
+@implementation BarcodeScannerViewController
+
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    if (self.scanner.isScanning) {
-        [self.scanner stopScanning];
-    }
-    [MTBBarcodeScanner requestCameraPermissionWithSuccess:^(BOOL success) {
-        if (success) {
-            [self startScan];
-        } else {
-          [self.delegate barcodeScannerViewController:self didFailWithErrorCode:@"PERMISSION_NOT_GRANTED"];
-          [self dismissViewControllerAnimated:NO completion:nil];
-        }
-    }];
+    [self.scanningView addTimer];
+    [_manager resetSampleBufferDelegate];
+
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
+    [self.scanningView removeTimer];
+    [_manager cancelSampleBufferDelegate];
+    
     if ([self isFlashOn]) {
         [self toggleFlash:NO];
     }
+    
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleDefault;//设置为黑色
+
+}
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}
+- (void)dealloc {
+    [self removeScanningView];
 }
 
-- (void)startScan {
-    NSError *error;
-    [self.scanner startScanningWithResultBlock:^(NSArray<AVMetadataMachineReadableCodeObject *> *codes) {
-        [self.scanner stopScanning];
-         AVMetadataMachineReadableCodeObject *code = codes.firstObject;
-        if (code) {
-            [self.delegate barcodeScannerViewController:self didScanBarcodeWithResult:code.stringValue];
-            [self dismissViewControllerAnimated:NO completion:nil];
-        }
-    } error:&error];
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor clearColor];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    [self.view addSubview:self.scanningView];
+    [self setupQRCodeScanning];
+    [self setupNavigationBar];
+    [self.view addSubview:self.promptLabel];
 }
 
-- (void)cancel {
+- (void)clickCloseButton {
+    [self.delegate barcodeScannerViewController:self didFailWithErrorCode:@"用户取消"];
     [self dismissViewControllerAnimated:true completion:nil];
+}
+
+- (SGQRCodeScanningView *)scanningView {
+    if (!_scanningView) {
+        _scanningView = [[SGQRCodeScanningView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    }
+    return _scanningView;
+}
+
+- (void)removeScanningView {
+    [self.scanningView removeTimer];
+    [self.scanningView removeFromSuperview];
+    self.scanningView = nil;
+}
+
+- (void)setupNavigationBar {
+    self.navigationItem.title = @"扫一扫";
+
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"关闭" style:UIBarButtonItemStylePlain target:self action:@selector(clickCloseButton)];
+    
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;//设置为白色
+
+    [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithWhite:0.1 alpha:1.0]];
+    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.navigationBar.shadowImage = [[UIImage alloc] init];
+
+    [self updateFlashButton];
+}
+
+- (void)setupQRCodeScanning {
+    self.manager = [SGQRCodeScanManager sharedManager];
+    NSArray *arr = @[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode128Code];
+//     AVCaptureSessionPreset1920x1080 推荐使用，对于小型的二维码读取率较高
+    [_manager setupSessionPreset:AVCaptureSessionPreset1920x1080 metadataObjectTypes:arr currentController:self];
+    [_manager cancelSampleBufferDelegate];
+
+    _manager.delegate = self;
+}
+
+#pragma mark - - - SGQRCodeAlbumManagerDelegate
+- (void)QRCodeAlbumManagerDidCancelWithImagePickerController:(SGQRCodeAlbumManager *)albumManager {
+    [self.view addSubview:self.scanningView];
+}
+
+- (void)QRCodeAlbumManagerDidReadQRCodeFailure:(SGQRCodeAlbumManager *)albumManager {
+    NSLog(@"暂未识别出二维码");
+}
+
+
+#pragma mark - - - SGQRCodeScanManagerDelegate
+- (void)QRCodeScanManager:(SGQRCodeScanManager *)scanManager didOutputMetadataObjects:(NSArray *)metadataObjects {
+    if (metadataObjects != nil && metadataObjects.count > 0) {
+        [scanManager playSoundName:@"SGQRCode.bundle/sound.caf"];
+        [scanManager stopRunning];
+        AVMetadataMachineReadableCodeObject *obj = metadataObjects[0];
+        
+        [self.delegate barcodeScannerViewController:self didScanBarcodeWithResult:obj.stringValue];
+
+         [self dismissViewControllerAnimated:true completion:nil];
+    
+    }else{
+//        [PClass showTheWarningContent:@"未识别"];
+        NSLog(@"暂未识别出二维码");
+    }
+}
+
+- (UILabel *)promptLabel {
+    if (!_promptLabel) {
+        _promptLabel = [[UILabel alloc] init];
+        _promptLabel.backgroundColor = [UIColor clearColor];
+        CGFloat promptLabelX = 0;
+        CGFloat promptLabelY = 0.73 * self.view.frame.size.height;
+        CGFloat promptLabelW = self.view.frame.size.width;
+        CGFloat promptLabelH = 25;
+        _promptLabel.frame = CGRectMake(promptLabelX, promptLabelY, promptLabelW, promptLabelH);
+        _promptLabel.textAlignment = NSTextAlignmentCenter;
+        _promptLabel.font = [UIFont boldSystemFontOfSize:13.0];
+        _promptLabel.textColor = [[UIColor whiteColor] colorWithAlphaComponent:0.6];
+        _promptLabel.text = @"将二维码/条码放入框内, 即可自动扫描";
+    }
+    return _promptLabel;
 }
 
 - (void)updateFlashButton {
@@ -72,15 +149,16 @@
         return;
     }
     if (self.isFlashOn) {
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Flash Off"
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"关闭闪光灯"
                                                                                   style:UIBarButtonItemStylePlain
                                                                                  target:self action:@selector(toggle)];
     } else {
-        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Flash On"
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"打开闪光灯"
                                                                                   style:UIBarButtonItemStylePlain
                                                                                  target:self action:@selector(toggle)];
     }
 }
+
 
 - (void)toggle {
     [self toggleFlash:!self.isFlashOn];
@@ -90,7 +168,7 @@
 - (BOOL)isFlashOn {
     AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     if (device) {
-        return device.torchMode == AVCaptureFlashModeOn || AVCaptureTorchModeOn;
+        return device.torchMode == AVCaptureFlashModeOn ||AVCaptureTorchModeOn;
     }
     return NO;
 }
@@ -106,7 +184,7 @@
 - (void)toggleFlash:(BOOL)on {
     AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     if (!device) return;
-
+    
     NSError *err;
     if (device.hasFlash && device.hasTorch) {
         [device lockForConfiguration:&err];
@@ -121,6 +199,5 @@
         [device unlockForConfiguration];
     }
 }
-
 
 @end
